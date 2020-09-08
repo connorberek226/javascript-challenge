959a8974663c47289d3bfcfe36759f661721e2a5
connorberek226 <64981067+connorberek226@users.noreply.github.com>
2020-09-07 19:26:34 -0500

    First adjustments made to the app.js files.

--- UFO-Level-1/static/js/app.js
+++ UFO-Level-1/static/js/app.js
@@ -1,4 +1,9 @@
 // from data.js
 var tableData = data;
 
+function listloop(tableData) {
+    for (var i = 0; i < tableData.length; i++) {
+        console.log(tableData[i]);
+    }
+}
 // YOUR CODE HERE!
